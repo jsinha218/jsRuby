333b07afaeb861ad0b4334e90daa7e0bfa136a84
jsinha218 <jsinha218@gmail.com>
2016-12-15 17:22:47 +0000

    Arrays in Ruby

---
+++ arraysJs.rb
@@ -0,0 +1,9 @@
+#Arrays indexed from 0 onwards
+#collection of data iyems of same type
+numbers = [1,2,3,4,5,6]
+print(numbers[0])
+print(numbers[3])
+numbers.each{|l|print(l.to_s)}
+
+names = ['richa','sharma','jennil','verma']
+names.each{|k|puts(k)}
